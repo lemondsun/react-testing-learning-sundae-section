ccc2ca3d7b03fc521940898a6a22c6dd789cf9be
Jason Mullings <wjasonmullings@gmail.com>
2021-05-30 15:44:16 -0400

    Code and test scoop options images from a server

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser')
+  worker.start()
+}
 
 ReactDOM.render(
   <React.StrictMode>
